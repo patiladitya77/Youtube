94d67cbfb67a12a91927d3982b9783f8a29ff14e
patiladitya77 <adpatil587@gmail.com>
2025-06-21 19:51:55 +0530

    video container and cards

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const VideoCard = ({ info }) => {
+  console.log(info);
+  const { title, channelTitle, thumbnails } = info.snippet;
+  return (
+    <div className="p-2 px-4 m-2 mx-4 w-72 shadow-lg">
+      <div>
+        <img className="rounded-lg " src={thumbnails.maxres.url} />
+        <ul>
+          <li className="font-bold py-2">{title}</li>
+          <li>{channelTitle}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCard;
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const [videos, setVidoes] = useState([]);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    setVidoes(json.items);
+  };
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((item) => (
+        <VideoCard key={item.id} info={item} />
+      ))}
+      {/* <VideoCard info={videos[0]} /> */}
+    </div>
+  );
 };
 
 export default VideoContainer;
